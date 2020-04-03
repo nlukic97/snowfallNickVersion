7eb87ce58f6c10dd814d607b1e7fadb36b004e3f
Nikola Lukic <56925545+nlukic97@users.noreply.github.com>
2020-04-03 20:05:57 +0200

    First commit

---
+++ Snow/js/script.js
@@ -0,0 +1,26 @@
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
+setInterval(function(){
+
+	var pahulja = $('.first').clone().removeClass('first');
+
+	var randomNo = Math.floor(Math.random() * windowWidth)
+
+	pahulja.css('left', randomNo).css('top', '-10px')
+
+	pahulja.appendTo('body').animate({
+	top: windowHeight,
+}, {
+	duration: 8000,
+
+	done: function(){
+		pahulja.remove();
+	}
+})
+},400)
+
+//dodaj animaciju da svaka leluja levo desno. Animiras njihov left sa random brojem izmedju -100 ili + 100px
+
+
+
