4ac30fd0e2ee8d0c74ea27a93433ac8584015455
nlukic97 <nlukic97@gmail.com>
2020-04-05 22:01:23 +0200

    e

---
+++ js/script.js
@@ -0,0 +1,143 @@
+/*var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
+setInterval(function(){
+
+	var pahulja = $('.first').clone().removeClass('first');
+
+	var randomNo = Math.floor(Math.random() * windowWidth)
+
+	pahulja.css('left', randomNo).css('top', '-10px')
+
+	pahulja.appendTo('body').animate({
+	top: windowHeight,
+}, {
+	duration: 8000,
+
+	done: function(){
+		pahulja.remove();
+	}
+})
+},400)*/
+
+//dodaj animaciju da svaka leluja levo desno. Animiras njihov left sa random brojem izmedju -100 ili + 100px
+
+/*var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+setInterval(function(){
+	var pahulja =  $('.first').clone().removeAttr('class')
+
+	var randomNo = Math.floor(Math.random() * (windowWidth))
+	var leftRight2 = Math.floor(Math.random() * 100);
+	pahulja.css('left', randomNo).css('top', '-15px')
+
+	pahulja.appendTo('body').animate({
+		top: windowHeight/4, 
+		left: '+=' + leftRight2
+	},
+	{
+		duration: 2000,
+		easing: "linear",
+		
+		done: function() {
+			
+			pahulja.animate({
+				top:windowHeight / 2,
+				left: '-=' + leftRight2 	
+			},{
+				duration:2000,
+				easing: "linear",
+
+				done: function() {
+					pahulja.animate({
+						top: windowHeight/4 * 3,
+						left: '+=' + leftRight2
+					}, {
+						duration: 2000,
+						easing: "linear",
+
+						done: function(){
+							pahulja.animate({
+								top: windowHeight,
+								left: '-=' + leftRight2
+							}, {
+								duration: 2000,
+								easing: "linear",
+								done: function(){
+									pahulja.remove()
+								}
+							})
+						}
+					})
+				}
+			});
+		}
+	})
+},200)*/
+
+
+
+
+//pomoc:      http://jsfiddle.net/scarqb7x/
+//origdjidji  http://jsfiddle.net/Xw29r/
+
+//ceo article https://stackoverflow.com/questions/10385950/how-to-get-a-div-to-randomly-move-around-a-page-using-jquery-or-css
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+var windowHeightQ = window.innerHeight / 4;
+
+setInterval(function(){
+	var pahulja =  $('.first').clone().removeAttr('class')
+
+	var randomNo = Math.floor(Math.random() * (windowWidth))
+	var leftRight2 = Math.floor(Math.random() * 100);
+	pahulja.css('left', randomNo).css('top', '-15px')
+
+	//da ide dole do kraja za 8 sekundi
+	pahulja.appendTo('body').animate({
+		top: windowHeight, 
+	},{
+		duration: 8000,
+		easing: 'linear',
+		queue: false, //ovo valjda pravi da ostalo krece odmah, i oni se redjaju jedni za drugim. It doesnt cue anything after this
+		done: function(){
+			pahulja.remove()
+		}
+	})
+
+	//2 sekunde desno
+	.animate({
+		left: '+=' + leftRight2,
+	},{
+		duration:2000,
+		easing:'linear',
+	})
+	//2 sekunde levo
+	.animate({
+		left: '-=' + leftRight2,
+	},{
+		duration:2000,
+		easing:'linear',
+	})
+
+	//2 sekunde desno
+	.animate({
+		left: '+=' + leftRight2,
+	},{
+		duration:2000,
+		easing:'linear',
+	})
+
+	//2 sekunde levo
+	.animate({
+		left: '-=' + leftRight2,
+	},{
+		duration:2000,
+		easing:'linear',
+	})
+},200)
+
+
+
