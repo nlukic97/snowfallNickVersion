82e9beda80d0d8e99c612c6820a14f37dff7a3e8
nlukic97 <nlukic97@gmail.com>
2020-04-06 10:17:35 +0200

    refactured code, solution 3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+////-------------- Postavka ------------------
+
 /*var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
 
@@ -22,6 +24,12 @@ setInterval(function(){
 
 //dodaj animaciju da svaka leluja levo desno. Animiras njihov left sa random brojem izmedju -100 ili + 100px
 
+
+
+
+
+
+//------------------------------------ Resenje 1--------------------------------- ------------------
 /*var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
 
@@ -84,7 +92,15 @@ setInterval(function(){
 
 //ceo article https://stackoverflow.com/questions/10385950/how-to-get-a-div-to-randomly-move-around-a-page-using-jquery-or-css
 
-var windowWidth = window.innerWidth;
+
+
+
+
+
+
+//----------------------------------------------- Resenje 2 ---------------------------------------------------
+
+/*var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
 var windowHeightQ = window.innerHeight / 4;
 
@@ -137,7 +153,64 @@ setInterval(function(){
 		duration:2000,
 		easing:'linear',
 	})
+},200)*/
+
+
+
+
+
+
+
+
+//----------------------------------------------- Resenje 3 ---------------------------------------------------
+
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+var windowHeightQ = window.innerHeight / 4;
+
+setInterval(function(){
+	var pahulja =  $('.first').clone().removeAttr('class')
+	var randomNo = Math.floor(Math.random() * (windowWidth))
+	
+	pahulja.css('left', randomNo).css('top', '-15px')
+
+	//animacija pahulje levo desno
+	function move(sign) {
+		var leftRight2 = Math.floor(Math.random() * 100); //daj random broj od 0 do 100
+		pahulja.animate({
+			left: sign + leftRight2,
+		},{
+			duration:2000,
+			easing:'linear',
+		}) 
+	}
+
+	//da ide dole do kraja za 8 sekundi
+	pahulja.appendTo('body').animate({
+		top: windowHeight, 
+	},{
+		duration: 8000,
+		easing: 'linear',
+		queue: false, //ovo valjda pravi da ostalo krece odmah, i oni se redjaju jedni za drugim. It doesnt cue anything after this
+		done: function(){
+			pahulja.remove()
+		}
+	})
+
+	//2 sekunde desno
+	move('+=')
+	//2 sekunde levo
+	move('-=')
+
+	//2 sekunde desno
+	move('+=')
+
+	//2 sekunde levo
+	move('-=')
 },200)
+
+
 
 
 
